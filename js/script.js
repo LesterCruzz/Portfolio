9555924bb5dc88d03e6fb362ef341e9767b20b08
LesterCruzz <cpe.cruz.marklester@gmail.com>
2023-10-07 16:36:39 +0800

    Color Change

--- js/script.js
+++ js/script.js
@@ -17,6 +17,44 @@ const bo__x = document.querySelector(".box3");
 
 const header = document.querySelector(".header");
 
+// Carousel
+
+const carousel = document.querySelector(".carousel"),
+firstImg = carousel.querySelectorAll("img")[0];
+arrowIcons = document.querySelectorAll(".featured__wrapper i");
+
+let isDragStart = false, prevPageX, prevScrollLeft;
+let firstImgWidth = firstImg.clientWidth + 14;
+
+arrowIcons.forEach(icon  => {
+  icon.addEventListener("click", () => {
+    carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+  });
+});
+
+const dragStart = (e) => {
+  isDragStart = true;
+  prevPageX = e.pageX;
+  prevScrollLeft = carousel.scrollLeft;
+}
+
+const dragging = (e) => {
+  if(!isDragStart) return;
+  e.preventDefault();
+  carousel.classList.add("dragging");
+  let positionDiff = e.pageX - prevPageX;
+  carousel.scrollLeft = prevScrollLeft - positionDiff;
+}
+
+const dragStop = () => {
+  isDragStart = false;
+  carousel.classList.remove("dragging");
+}
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("mouseup", dragStop);
+
 toggleBtn.addEventListener("click", () =>{
     headerNav.classList.toggle("open");
     toggleBtn.classList.toggle("open"); 
